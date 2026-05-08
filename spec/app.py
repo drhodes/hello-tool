d2f450df1577f423ed7502d790faed6659934887
Derek Rhodes <physci@gmail.com>
2026-05-08 16:44:14 -0700

    speccing out xpath tool

--- spec/app.py
+++ spec/app.py
@@ -21,10 +21,23 @@ class Hello(Tool):
 class HelloPython(Tool):
     '''
     This tool should shell out to the command line and run:
-
     $ uv run main.py
 
-    From the project root: /usr/backup-working/work/hello-tool
-
+    From the project root: 
     The tool should have filename: "hello-python-tool.ts"
+    '''
+
+
+
+class HelloXPath(Tool):
+    '''This tool should shell out to the command line and run:
+    $ uv run xpath.py <query>
+
+    The tool should open a random .xml file from the directory
+
+    ./spec-build/spec-4236519a509c9e07ac8b.xml
+
+    Then it should run the query on it.
+    
+    The tool should have filename: "hello-xpath.ts"
     '''
